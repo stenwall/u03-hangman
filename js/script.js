72c9c64929c5768026bc2188c5bb01518f7a61f9
Karin <karin.stenwall@gmail.com>
2020-11-06 15:29:23 +0100

    event listener for start button

--- js/script.js
+++ js/script.js
@@ -11,12 +11,17 @@ const wordList = [
   "vatten",
 ];
 // DOM-nod: knappen som du startar spelet med
-let startGameBtnEl = document.querySelector('#startGameBtn');
+let startGameBtnEl = document.querySelector("#startGameBtn");
+// event listener för startknappen
+startGameBtnEl.addEventListener("click", () => {
+  alert("hej");
+})
 
 // Array av DOM-noder: Knapparna för bokstäverna
 let letterButtonEls = document.querySelectorAll("#letterButtons button");
 
-let letterBoxEls; // Array av DOM-noder: Rutorna där bokstäverna ska stå
+// Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterBoxEls = document.querySelectorAll("#letterBoxes > ul");
 
 // function to generate random word:
 function generateRandomWord(arr) {
@@ -26,15 +31,16 @@ function generateRandomWord(arr) {
 
 // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
 let selectedWord = generateRandomWord(wordList);
-console.log(letterButtonEls);
+
+
+
+// TESTA OM DET FUNKAR HÄR
+console.log(letterBoxEls);
 
 let guesses = 0; // Number: håller antalet gissningar som gjorts
 let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
 let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
-
-
-
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 
@@ -42,5 +48,3 @@ let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
-
-
