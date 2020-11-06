7da6768bb5948e891de023aa5601b3e1a6c209c5
Karin <karin.stenwall@gmail.com>
2020-11-06 14:15:18 +0100

    temp wordlist

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-// Globala variabler
+// temp wordlist, making a longer one in seperate file later
+const wordList = [
+  "kaffe",
+  "te",
+  "mjölk",
+  "glögg",
+  "vin",
+  "läsk",
+  "soda",
+  "juice",
+  "vatten",
+];
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+let selectedWord; // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
 
-let guesses = 0;     // Number: håller antalet gissningar som gjorts
-let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+let guesses = 0; // Number: håller antalet gissningar som gjorts
+let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
+let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
+let startGameBtnEl; // DOM-nod: knappen som du startar spelet med
 let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterBoxEls; // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 // Funktion som slumpar fram ett ord
