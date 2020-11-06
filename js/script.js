4c697f81cc2417c7eba04467de7eecd91f486618
Karin <karin.stenwall@gmail.com>
2020-11-06 14:47:19 +0100

    add DOM-nodes in variables for buttons

--- js/script.js
+++ js/script.js
@@ -10,20 +10,37 @@ const wordList = [
   "juice",
   "vatten",
 ];
+// DOM-nod: knappen som du startar spelet med
+let startGameBtnEl = document.querySelector('#startGameBtn');
 
-let selectedWord; // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+// Array av DOM-noder: Knapparna för bokstäverna
+let letterButtonEls = document.querySelectorAll("#letterButtons button");
+
+let letterBoxEls; // Array av DOM-noder: Rutorna där bokstäverna ska stå
+
+// function to generate random word:
+function generateRandomWord(arr) {
+  const randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
+}
+
+// Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+let selectedWord = generateRandomWord(wordList);
+console.log(letterButtonEls);
 
 let guesses = 0; // Number: håller antalet gissningar som gjorts
 let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
 let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl; // DOM-nod: knappen som du startar spelet med
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls; // Array av DOM-noder: Rutorna där bokstäverna ska stå
+
+
+
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
+
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+
+
