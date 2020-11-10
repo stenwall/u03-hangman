841067c13f2abd426ca299de6d376426c90f5baa
Karin <karin.stenwall@gmail.com>
2020-11-10 22:09:52 +0100

    add working start button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Variables that are needed in the global scope
+
 // temp wordlist, making a longer one in seperate file later
 const wordList = [
   "kaffe",
@@ -10,64 +12,62 @@ const wordList = [
   "juice",
   "vatten",
 ];
-// Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+// save the randomly generated word
 let selectedWord = null;
+// number of guesses made
+let guesses = 0;
+// hangman image
+let hangmanImg;
+// DOM-node: where messages show up
+let msgHolderEl = document.querySelector("#message");
+// DOM-node: button to start game
+let startGameBtnEl = document.querySelector("#startGameBtn");
+// DOM-nodeList: letter buttons
+let letterButtonEls = document.querySelectorAll("#letterButtons button");
+// DOM-nodeList: letter boxes
+let letterBoxEls = document.querySelectorAll("#letterBoxes li");
+// DOM-node: letter box container
+let letterBoxContainerEl = document.querySelector("#letterBoxes ul");
+
+// delete letter boxes when new game starts
+function clearLetterBoxes() {
+  letterBoxContainerEl.innerHTML = "";
+}
+
+// create a new <li>-element containing a <input>
+// Use .appendChild() to add the new element to letterBoxEls
+function createLetterBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    let newLiEl = document.createElement("LI");
+    newLiEl.innerHTML = '<input type="text" disabled value="&nbsp;"/>';
+    letterBoxContainerEl.appendChild(newLiEl);
+  }
+  letterBoxEls = document.querySelectorAll("#letterBoxes li");
+  console.log(letterBoxEls);
+}
+
 // function to generate random word:
 function generateRandomWord(arr) {
   const randomNumber = Math.floor(Math.random() * arr.length);
   return arr[randomNumber];
 }
-// DOM-nod: knappen som du startar spelet med
-let startGameBtnEl = document.querySelector("#startGameBtn");
-// event listener för startknappen
+
+// event listener for start button
 startGameBtnEl.addEventListener("click", startGame);
+
 // callback function for event listener
 function startGame() {
+  clearLetterBoxes();
   selectedWord = generateRandomWord(wordList);
-  createLetterBoxes();
+  let selectedWordLength = selectedWord.length;
+  createLetterBoxes(selectedWordLength);
 }
 
-function createLetterBoxes(arr) {
-
-}
-
-
-function doHomework(subject, callback) {
-  alert(`Starting my ${subject} homework.`);
-  callback();
-}
-function alertFinished() {
-  alert("Finished my homework");
-}
-doHomework("math", alertFinished);
-
-// Node List: Array av DOM-noder - Knapparna för bokstäverna
-let letterButtonEls = document.querySelectorAll("#letterButtons button");
-
-// Node List: Array av DOM-noder - Rutorna där bokstäverna ska stå
-// OBS! denna innehåller just nu bara ul, fast det är en nodelist
-let letterBoxEls = document.querySelectorAll("#letterBoxes ul");
-
-
-
-
-
-// function callback() {
-//   console.log('hej jag är en callback');
-// }
-// function anropaFunktion(minfunktion) {
-//   minfunktion()
-// }
-
-// anropaFunktion(callback);
+// event lyssnaren ska kalla på bl a createLetterBoxes()
+// 1.
 
 // TESTA OM DET FUNKAR HÄR
-console.log(letterBoxEls);
-
-let guesses = 0; // Number: håller antalet gissningar som gjorts
-let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
-
-let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
+// console.log(letterBoxEls);
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 
